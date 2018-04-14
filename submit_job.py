046cc5436e44fe521eadda726abe2f63ca102361
Pengcheng Yin <pcyin@cs.cmu.edu>
2018-04-14 19:21:19 -0400

    added argparse

--- submit_job.py
+++ submit_job.py
@@ -1,23 +1,26 @@
-#!/projects/tir1/users/pengchey/anaconda2/bin/python
+#!/usr/bin/env python
 from __future__ import print_function
 from sh import sbatch
 import sh
+import argparse
 import sys
 
-if len(sys.argv) <= 2:
-	print('USAGE: python submit_job.py JOB_NAME job_script1.sh job_script2.sh')
-	exit(1)
+parser = argparse.ArgumentParser()
+parser.add_argument("job_scripts", nargs='+', help='a list of job scripts to execute')
+parser.add_argument("--job_name", help='job name', type=str, required=True)
+parser.add_argument("--mem_per_job", help='amount of memory allocated per job', type=int, default=10000)
+parser.add_argument('--cpus_per_jobs', help='num. cpus per job', type=int, default=1)
+parser.add_argument('--output', default='output.out', type=str)
 
-job_name = sys.argv[1]
-job_scripts = sys.argv[2:]
-job_num = len(job_scripts)
+args = parser.parse_args()
+job_num = len(args.job_scripts)
 
 jobs = []
-print('submitting batched job: %s' % job_name)
-for job_script in job_scripts:
+print('submitting batched job [%s], %d sub-jobs' % (args.job_name, job_num))
+for job_script in args.job_scripts:
 	print('\tsubmitting: %s' % job_script)
 
-slurm_script = """#!{path_to_python}
+slurm_script = """#!/usr/bin/env python
 
 import sh, sys
 jobs = []
@@ -26,15 +29,14 @@ for job_script in {job_scripts}:
 	jobs.append(job)
 
 for job in jobs: job.wait()
-""".format(path_to_python=sh.which('python'), 
-		   job_scripts='[%s]' % ', '.join(['"%s"' % job_script for job_script in job_scripts]))
+""".format(job_scripts='[%s]' % ', '.join(['"%s"' % job_script for job_script in args.job_scripts]))
 
 job = sbatch('--gres', 'gpu:1',
-             '--job-name', job_name,
-             '--mem', 10000 * job_num,  # memory
-             '--cpus-per-task', job_num,  # number of cpus
+             '--job-name', args.job_name,
+             '--mem', args.mem_per_job * job_num,  # memory
+             '--cpus-per-task', args.cpus_per_jobs * job_num,  # number of cpus
              '--time', 0,  # wait time: unlimited
-             '--output', 'output.out',  # assume you don't need stdout
+             '--output', args.output,  # assume you don't need stdout
              _in=slurm_script)
 
 print(job.stdout)
